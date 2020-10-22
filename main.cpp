10fec5c466ff96f33026e2bb962509ec2f8be983
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-10-22 11:53:37 +0300

    test background for window

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <unistd.h>
 
+#include "data_classes/data_classes.hpp"
 #include "window/window.hpp"
 
 
 int main() {
     Renderer renderer (Size(1920, 1080));
 
-    RectWindow window(Size(50, 20), Position(10, 10), Color(255, 0, 0));
+    RectWindow window(Size(1920, 900), Position(0, 0), Color(176, 176, 176));
    
     window.open(); 
     window.render(renderer);
--- ncurses_engine/ncurses_engine.cpp
+++ ncurses_engine/ncurses_engine.cpp
@@ -19,10 +19,11 @@ void Renderer::draw_rectangle(Size size, Position pos, Color color) {
     int win_pos_x = pos.x / (2 * scale_x);
     int win_pos_y = pos.y / scale_y;
 
-    WINDOW* rect = newwin(win_size_y, win_size_x, win_pos_y, win_size_x);
+
+    WINDOW* rect = newwin(win_size_y, win_size_x, win_pos_y, win_pos_x);
 
     init_color(16, color.r, color.b, color.g);
-    init_pair(1, COLOR_BLUE, 16); // how to add more colors?
+    init_pair(1, COLOR_BLUE, 16);
     wbkgd(rect, ' ' | COLOR_PAIR(1));
     touchwin(rect);
     refresh();
