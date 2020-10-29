99c6863f51aec9cd03de907999f630bfb2094445
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-10-30 02:04:28 +0300

    finished window base class separation

--- window/window.cpp
+++ window/window.cpp
@@ -21,26 +21,6 @@ void InterfaceClickable::handle_mouse_button_event(Event* event) {
 InterfaceClickable::~InterfaceClickable() = default;
 
 /*---------------------------------------*/
-/*              Window                   */
-/*---------------------------------------*/
-
-Window::Window() = default;
-
-void Window::set_event_mask(uint32_t mask) { event_mask = mask; }
-
-void Window::add_child_window(std::unique_ptr<Window>& child) {
-    subwindows.push_back(std::move(child));
-}
-
-Window::~Window() = default;
-
-void Window::handle_event(Event* event) {
-    for (auto& subwindow : subwindows) {
-        subwindow->handle_event(event);
-    }
-}
-
-/*---------------------------------------*/
 /*            RenderWindow               */
 /*---------------------------------------*/
 
