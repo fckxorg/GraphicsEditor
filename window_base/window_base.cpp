34caf694717812afb9c35d0ca38451a43e819c57
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-01 16:37:52 +0300

    window base manager refactored

--- window_base/window_base.cpp
+++ window_base/window_base.cpp
@@ -6,13 +6,11 @@
 
 Window::Window() = default;
 
-
 void Window::add_child_window(std::unique_ptr<Window>& child) {
-    subwindows.push_back(std::move(child));
+  subwindows.push_back(std::move(child));
 }
 
 Window::~Window() = default;
 
 void Window::handle_event(Event* event) {}
-
 
--- window_base/window_base.hpp
+++ window_base/window_base.hpp
@@ -1,23 +1,22 @@
 #ifndef WINDOW_BASE_HPP
 #define WINDOW_BASE_HPP
 
-
+#include <cstdint>
 #include <list>
 #include <memory>
-#include <cstdint>
 
 #include "../event/event.hpp"
 
 class Window {
-   public:
-    std::list<std::unique_ptr<Window>> subwindows;
+ public:
+  std::list<std::unique_ptr<Window>> subwindows;
 
-    Window();
-    virtual ~Window();
+  Window();
+  virtual ~Window();
 
-    void add_child_window(std::unique_ptr<Window>& child);
-    virtual void handle_event(Event* event);
-    virtual void render() = 0;
+  void add_child_window(std::unique_ptr<Window>& child);
+  virtual void handle_event(Event* event);
+  virtual void render() = 0;
 };
 
 #endif
