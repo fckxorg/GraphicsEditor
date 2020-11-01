cbd026f8249fd7b1f32519299fe482c4a168ba5b
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-01 16:35:59 +0300

    subscription manager refactored

--- subscription_manager/subscription_manager.cpp
+++ subscription_manager/subscription_manager.cpp
@@ -4,29 +4,42 @@ std::unordered_map<Window*, std::unordered_set<Window*>>
     SubscriptionManager::subscriptions;
 
 void SubscriptionManager::add_subscription(Window* sender, Window* recipient) {
-    subscriptions[sender].insert(recipient);
+  assert(sender != nullptr);
+  assert(recipient != nullptr);
+
+  subscriptions[sender].insert(recipient);
 }
 
 void SubscriptionManager::unsubscribe(Window* sender, Window* recipient) {
-    subscriptions[sender].erase(recipient);
+  assert(sender != nullptr);
+  assert(recipient != nullptr);
+
+  subscriptions[sender].erase(recipient);
 }
 
 void SubscriptionManager::unsubscribe_all(Window* sender) {
-    subscriptions[sender].clear();
+  assert(sender != nullptr);
+
+  subscriptions[sender].clear();
 }
 
 void SubscriptionManager::unsubscribe_from_all(Window* recipient) {
-    for (auto& subscription : subscriptions) {
-        if (subscription.second.contains(recipient)) {
-            subscription.second.erase(recipient);
-        }
+  assert(recipient != nullptr);
+
+  for (auto& subscription : subscriptions) {
+    if (subscription.second.contains(recipient)) {
+      subscription.second.erase(recipient);
     }
+  }
 }
 
 void SubscriptionManager::send_event(Window* sender, Event* event) {
-    for (auto& recipient : subscriptions[sender]) {
-        recipient->handle_event(event);
-    }
+  assert(sender != nullptr);
+  assert(event != nullptr);
 
-    delete event;
+  for (auto& recipient : subscriptions[sender]) {
+    recipient->handle_event(event);
+  }
+
+  delete event;
 }
--- subscription_manager/subscription_manager.hpp
+++ subscription_manager/subscription_manager.hpp
@@ -1,22 +1,24 @@
 #ifndef SUBSCRIPTION_MANAGER_HPP
 #define SUBSCRIPTION_MANAGER_HPP
 
+#include <cstdarg>
 #include <unordered_map>
 #include <unordered_set>
-#include <cstdarg>
+#include <cassert>
 
 #include "../window_base/window_base.hpp"
 
 class SubscriptionManager {
-    private:
-        static std::unordered_map<Window*, std::unordered_set<Window*>> subscriptions;
-    public:
-        SubscriptionManager() = delete;
-        static void add_subscription(Window* sender, Window* recipient);
-        static void unsubscribe(Window* sender, Window* recipient);
-        static void unsubscribe_all(Window* sender);
-        static void unsubscribe_from_all(Window* recipient);
-        static void send_event(Window* sender, Event* event);
+ private:
+  static std::unordered_map<Window*, std::unordered_set<Window*>> subscriptions;
+
+ public:
+  SubscriptionManager() = delete;
+  static void add_subscription(Window* sender, Window* recipient);
+  static void unsubscribe(Window* sender, Window* recipient);
+  static void unsubscribe_all(Window* sender);
+  static void unsubscribe_from_all(Window* recipient);
+  static void send_event(Window* sender, Event* event);
 };
 
 #endif
