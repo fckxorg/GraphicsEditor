0a8924ee56f0e6b134280bc0b5021b9a8e79bbf3
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-01 16:30:22 +0300

    event_queue refactoring

--- event_queue/event_queue.cpp
+++ event_queue/event_queue.cpp
@@ -4,9 +4,10 @@ std::queue<Event*> EventQueue::event_queue;
 
 bool EventQueue::empty() { return event_queue.empty(); }
 
-void EventQueue::add_event(Event* new_event) { 
-    assert(new_event != nullptr);
-    event_queue.push(new_event); }
+void EventQueue::add_event(Event* new_event) {
+  assert(new_event != nullptr);
+  event_queue.push(new_event);
+}
 
 Event* EventQueue::get_event() {
   assert(!event_queue.empty());
