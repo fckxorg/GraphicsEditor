b744f1cf284c76b009c1ab569bd1bfb1936df243
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-01 16:12:56 +0300

    code style changes in main and event

--- event/event.cpp
+++ event/event.cpp
@@ -1,21 +1,20 @@
 #include "event.hpp"
 
-uint32_t Event::get_type() {
-    return type;
-}
+uint32_t Event::get_type() { return type; }
 
 Event::Event(uint32_t type) : type(type) {}
 
 Event::~Event() = default;
 
-
 WindowClosedEvent::WindowClosedEvent() : Event(WINDOW_CLOSED) {}
 
-MouseButtonEvent::MouseButtonEvent(Position pos, MouseButton button, Action action) : Event(MOUSE_BUTTON), pos(pos), button(button), action(action) {}
+MouseButtonEvent::MouseButtonEvent(Position pos, MouseButton button,
+                                   Action action)
+    : Event(MOUSE_BUTTON), pos(pos), button(button), action(action) {}
 
+MouseMoveEvent::MouseMoveEvent(Position pos) : Event(MOUSE_MOVE), pos(pos) {}
 
-MouseMoveEvent::MouseMoveEvent(Position pos) : Event(MOUSE_MOVE), pos(pos){}
-
-ButtonPressEvent::ButtonPressEvent(uint32_t value) : Event(BUTTON_PRESSED), value(value) {}
+ButtonPressEvent::ButtonPressEvent(uint32_t value)
+    : Event(BUTTON_PRESSED), value(value) {}
 
 ScrollEvent::ScrollEvent(int delta) : Event(SCROLL), delta(delta) {}
--- event/event.hpp
+++ event/event.hpp
@@ -5,53 +5,59 @@
 
 #include "../data_classes/data_classes.hpp"
 
-enum SYSTEM_EVENT { MOUSE_BUTTON, MOUSE_MOVE, WINDOW_CLOSED, BUTTON_PRESSED, SCROLL };
+enum SYSTEM_EVENT {
+  MOUSE_BUTTON,
+  MOUSE_MOVE,
+  WINDOW_CLOSED,
+  BUTTON_PRESSED,
+  SCROLL
+};
 
 class Event {
-   private:
-    uint32_t type;
+ private:
+  uint32_t type;
 
-   public:
-    uint32_t get_type();
-    Event();
+ public:
+  uint32_t get_type();
+  Event();
 
-    Event(uint32_t type);
-    Event(const Event& event);
-    virtual ~Event();
+  Event(uint32_t type);
+  Event(const Event& event);
+  virtual ~Event();
 };
 
 class WindowClosedEvent : public Event {
-   public:
-    WindowClosedEvent();
+ public:
+  WindowClosedEvent();
 };
 
 class MouseButtonEvent : public Event {
-   public:
-    enum MouseButton { LEFT, MIDDLE, RIGHT, UNDEFINED_BUTTON };
-    enum Action { PRESSED, RELEASED };
-    Position pos;
-    MouseButton button;
-    Action action;
-    MouseButtonEvent(Position pos, MouseButton button, Action action);
+ public:
+  enum MouseButton { LEFT, MIDDLE, RIGHT, UNDEFINED_BUTTON };
+  enum Action { PRESSED, RELEASED };
+  Position pos;
+  MouseButton button;
+  Action action;
+  MouseButtonEvent(Position pos, MouseButton button, Action action);
 };
 
 class MouseMoveEvent : public Event {
-   public:
-    Position pos;
+ public:
+  Position pos;
 
-    MouseMoveEvent(Position pos);
+  MouseMoveEvent(Position pos);
 };
 
 class ButtonPressEvent : public Event {
-   public:
-    uint32_t value;
-    ButtonPressEvent(uint32_t value);
+ public:
+  uint32_t value;
+  ButtonPressEvent(uint32_t value);
 };
 
 class ScrollEvent : public Event {
-    public:
-        int delta;
-        ScrollEvent(int delta); 
+ public:
+  int delta;
+  ScrollEvent(int delta);
 };
 
 #endif
