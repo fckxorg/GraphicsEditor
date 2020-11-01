b2a32f55218d27a984ea0b32328e0e3d06ba0040
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-01 16:40:50 +0300

    amal enhancments in sfml_engine

--- sfml_engine/sfml_engine.cpp
+++ sfml_engine/sfml_engine.cpp
@@ -1,70 +1,63 @@
 #include "sfml_engine.hpp"
 
-#include <stdio.h>
-
-#include <SFML/Graphics/Color.hpp>
-#include <SFML/Graphics/RenderTexture.hpp>
-#include <SFML/Graphics/Sprite.hpp>
-#include <unordered_map>
-
 sf::RenderWindow Renderer::window;
 std::unordered_map<const char*, sf::Font> Renderer::fonts;
 
 void Renderer::clear() { window.clear(); }
 
 void Renderer::init(Size window_size, const char* name) {
-    window.create(sf::VideoMode(window_size.width, window_size.height), name);
-    Renderer::clear();
+  assert(name != nullptr);
+
+  window.create(sf::VideoMode(window_size.width, window_size.height), name);
+  Renderer::clear();
 }
 
 void Renderer::draw_rectangle(Size size, Position pos, Color color) {
-    sf::RectangleShape rect = sf::RectangleShape(size);
-    rect.setPosition(pos);
-    rect.setFillColor(color);
-    window.draw(rect);
+  sf::RectangleShape rect = sf::RectangleShape(size);
+  rect.setPosition(pos);
+  rect.setFillColor(color);
+  window.draw(rect);
 }
 
 void Renderer::draw_text(Text text, Position pos, Color bg_color) {
-    if (!fonts.contains(text.font_path)) {
-        sf::Font new_font;
-        new_font.loadFromFile(text.font_path);
-        fonts[text.font_path] = new_font;
-    }
+  if (!fonts.contains(text.font_path)) {
+    sf::Font new_font;
+    new_font.loadFromFile(text.font_path);
+    fonts[text.font_path] = new_font;
+  }
 
-    sf::Text sfml_text(text.text, fonts[text.font_path],
-                       text.character_size);
-    sfml_text.setPosition(pos);
-    sfml_text.setLineSpacing(text.line_spacing);
-    sfml_text.setFillColor(text.color);
+  sf::Text sfml_text(text.text, fonts[text.font_path], text.character_size);
+  sfml_text.setPosition(pos);
+  sfml_text.setLineSpacing(text.line_spacing);
+  sfml_text.setFillColor(text.color);
 
-    window.draw(sfml_text);
+  window.draw(sfml_text);
 }
 
 void Renderer::draw_scrollable_text(Text text, Size size, Position pos,
                                     Color color, float relative_offset) {
-    if (!fonts.contains(text.font_path)) {
-        sf::Font new_font;
-        new_font.loadFromFile(text.font_path);
-        fonts[text.font_path] = new_font;
-    }
+  if (!fonts.contains(text.font_path)) {
+    sf::Font new_font;
+    new_font.loadFromFile(text.font_path);
+    fonts[text.font_path] = new_font;
+  }
 
-    sf::Text sfml_text(text.text, fonts[text.font_path],
-                       text.character_size);
-    sfml_text.setPosition(0, relative_offset);
-    sfml_text.setFillColor(text.color);
-    sfml_text.setLineSpacing(text.line_spacing);
+  sf::Text sfml_text(text.text, fonts[text.font_path], text.character_size);
+  sfml_text.setPosition(0, relative_offset);
+  sfml_text.setFillColor(text.color);
+  sfml_text.setLineSpacing(text.line_spacing);
 
-    sf::RenderTexture viewport_texture;
-    viewport_texture.create(size.width, size.height);
-    viewport_texture.clear(sf::Color::White);
+  sf::RenderTexture viewport_texture;
+  viewport_texture.create(size.width, size.height);
+  viewport_texture.clear(sf::Color::White);
 
-    viewport_texture.draw(sfml_text);
-    viewport_texture.display();
+  viewport_texture.draw(sfml_text);
+  viewport_texture.display();
 
-    sf::Sprite viewport_sprite;
-    viewport_sprite.setTexture(viewport_texture.getTexture());
-    viewport_sprite.setPosition(pos);
-    window.draw(viewport_sprite);
+  sf::Sprite viewport_sprite;
+  viewport_sprite.setTexture(viewport_texture.getTexture());
+  viewport_sprite.setPosition(pos);
+  window.draw(viewport_sprite);
 }
 
 void Renderer::deinit() { window.close(); }
@@ -73,54 +66,54 @@ void Renderer::show() { window.display(); }
 
 MouseButtonEvent::MouseButton Renderer::get_mouse_button(
     sf::Mouse::Button button) {
-    switch (button) {
-        case sf::Mouse::Left:
-            return MouseButtonEvent::MouseButton::LEFT;
-        case sf::Mouse::Right:
-            return MouseButtonEvent::MouseButton::RIGHT;
-        case sf::Mouse::Middle:
-            return MouseButtonEvent::MouseButton::MIDDLE;
-        default:
-            return MouseButtonEvent::MouseButton::UNDEFINED_BUTTON;
-    }
+  switch (button) {
+    case sf::Mouse::Left:
+      return MouseButtonEvent::MouseButton::LEFT;
+    case sf::Mouse::Right:
+      return MouseButtonEvent::MouseButton::RIGHT;
+    case sf::Mouse::Middle:
+      return MouseButtonEvent::MouseButton::MIDDLE;
+    default:
+      return MouseButtonEvent::MouseButton::UNDEFINED_BUTTON;
+  }
 }
 
 Event* Renderer::translateMouseEvent(sf::Event::MouseButtonEvent sf_mouse_data,
                                      MouseButtonEvent::Action action) {
-    Position pos(sf_mouse_data.x, sf_mouse_data.y);
-    MouseButtonEvent::MouseButton button =
-        Renderer::get_mouse_button(sf_mouse_data.button);
+  Position pos(sf_mouse_data.x, sf_mouse_data.y);
+  MouseButtonEvent::MouseButton button =
+      Renderer::get_mouse_button(sf_mouse_data.button);
 
-    MouseButtonEvent* event = new MouseButtonEvent(pos, button, action);
+  MouseButtonEvent* event = new MouseButtonEvent(pos, button, action);
 
-    return event;
+  return event;
 }
 
 Event* Renderer::poll_event() {
-    sf::Event sf_event;
+  sf::Event sf_event;
 
-    if (!window.pollEvent(sf_event)) return nullptr;
+  if (!window.pollEvent(sf_event)) return nullptr;
 
-    switch (sf_event.type) {
-        case sf::Event::Closed: {
-            return new WindowClosedEvent();
-        }
-        case sf::Event::MouseButtonPressed: {
-            return Renderer::translateMouseEvent(
-                sf_event.mouseButton, MouseButtonEvent::Action::PRESSED);
-        }
-
-        case sf::Event::MouseButtonReleased: {
-            return Renderer::translateMouseEvent(
-                sf_event.mouseButton, MouseButtonEvent::Action::RELEASED);
-        }
-
-        case sf::Event::MouseMoved: {
-            Position pos(sf_event.mouseMove.x, sf_event.mouseMove.y);
-            return new MouseMoveEvent(pos);
-        }
+  switch (sf_event.type) {
+    case sf::Event::Closed: {
+      return new WindowClosedEvent();
+    }
+    case sf::Event::MouseButtonPressed: {
+      return Renderer::translateMouseEvent(sf_event.mouseButton,
+                                           MouseButtonEvent::Action::PRESSED);
     }
 
-    return nullptr;
+    case sf::Event::MouseButtonReleased: {
+      return Renderer::translateMouseEvent(sf_event.mouseButton,
+                                           MouseButtonEvent::Action::RELEASED);
+    }
+
+    case sf::Event::MouseMoved: {
+      Position pos(sf_event.mouseMove.x, sf_event.mouseMove.y);
+      return new MouseMoveEvent(pos);
+    }
+  }
+
+  return nullptr;
 }
 
--- sfml_engine/sfml_engine.hpp
+++ sfml_engine/sfml_engine.hpp
@@ -1,42 +1,46 @@
 #ifndef SFML_ENGINE_HPP
 #define SFML_ENGINE_HPP
 
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RectangleShape.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/System/Vector2.hpp>
-#include <SFML/Graphics/Font.hpp>
-#include <SFML/Window/VideoMode.hpp>
 #include <SFML/Window/Event.hpp>
-
+#include <SFML/Window/VideoMode.hpp>
 #include <unordered_map>
+#include <cassert>
 
 #include "../data_classes/data_classes.hpp"
 #include "../event/event.hpp"
 
 class Renderer {
-   private:
-    static sf::RenderWindow window;
+ private:
+  static sf::RenderWindow window;
 
-    static std::unordered_map<const char*, sf::Font> fonts;
+  static std::unordered_map<const char*, sf::Font> fonts;
 
-    static MouseButtonEvent::MouseButton get_mouse_button(sf::Mouse::Button);
-    static Event* translateMouseEvent(sf::Event::MouseButtonEvent sf_mouse_data, MouseButtonEvent::Action action);
+  static MouseButtonEvent::MouseButton get_mouse_button(sf::Mouse::Button);
+  static Event* translateMouseEvent(sf::Event::MouseButtonEvent sf_mouse_data,
+                                    MouseButtonEvent::Action action);
 
-    Renderer();
+  Renderer();
 
-   public:
-    static void clear();
-    static void draw_rectangle(Size size, Position pos, Color color); 
-    static void draw_text(Text text, Position pos, Color bg_color);
-    static void draw_scrollable_text(Text text, Size size, Position pos, Color color, float relative_offset);
-    static void init(Size window_size, const char* name);
-    static void show();
-    static void deinit();
+ public:
+  static void init(Size window_size, const char* name);
+  static void deinit();
 
-    static Event* poll_event();
+  static void clear();
+  static void show();
+
+  static Event* poll_event();
+
+  static void draw_rectangle(Size size, Position pos, Color color);
+  static void draw_text(Text text, Position pos, Color bg_color);
+  static void draw_scrollable_text(Text text, Size size, Position pos,
+                                   Color color, float relative_offset);
+
 };
 #endif
