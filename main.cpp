90e6cee896e6ae497674564e30c8bf0a5f38567f
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-25 14:32:46 +0300

    finished sprites creation

--- main.cpp
+++ main.cpp
@@ -20,14 +20,22 @@ int main() {
 
   std::unique_ptr<Window> root_window(new RootWindow());
   std::unique_ptr<Window> window(
-      new RectWindow(Size(1920, 900), Position(0, 0), Color(255, 255, 255)));
+      new RectWindow(Size(1920, 1080), Position(0, 0), Color(212, 212, 212)));
+
+  std::unique_ptr<Window> pencil_button_sprite(
+      new Sprite(Texture("icons/pencil.png", Size(50, 50)), Position(10, 910)));
+  std::unique_ptr<Window> eraser_button_sprite(
+      new Sprite(Texture("icons/eraser.png", Size(50, 50)), Position(10, 965)));
 
   std::unique_ptr<Window> toolbar_listener(new ToolbarListener());
 
-  std::unique_ptr<Window> pencil_button(
-      new RectButton(Size(50, 50), Position(0, 400), Color(0, 255, 0), PENCIL));
-  std::unique_ptr<Window> eraser_button(
-      new RectButton(Size(50, 50), Position(0, 455), Color(255, 0, 0), ERASER));
+  std::unique_ptr<Window> pencil_button(new RectButton(
+      Size(50, 50), Position(10, 910), Color(236, 236, 236), PENCIL));
+  std::unique_ptr<Window> eraser_button(new RectButton(
+      Size(50, 50), Position(10, 965), Color(236, 236, 236), ERASER));
+
+  pencil_button->add_child_window(pencil_button_sprite);
+  eraser_button->add_child_window(eraser_button_sprite);
 
   SUBSCRIBE(root_window.get(), pencil_button.get());
   SUBSCRIBE(root_window.get(), eraser_button.get());
