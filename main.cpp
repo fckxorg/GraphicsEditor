423453d9ddca72caaccca72a5e3df8afd515ffb6
Maxim Kokryashkin <max.kokryashkin@gmail.com>
2020-11-26 01:19:19 +0300

    rewritten main with CREATE macro

--- main.cpp
+++ main.cpp
@@ -21,55 +21,52 @@ int main() {
     return -1;
   }
 
-
+  /* LAYOUT DEFENITION */
   CREATE(root_window, RootWindow);
   CREATE(window, RectWindow, Size(1920, 1080), Position(0, 0),
          Color(212, 212, 212));
 
-  CREATE(pencil_button_sprite, Sprite,
-         Texture("icons/pencil.png", Size(50, 50)), Position(15, 910));
-  CREATE(eraser_button_sprite, Sprite, Texture("icons/eraser.png", Size(50, 50)), Position(15, 980));
-  CREATE(save_button_sprite, Sprite, Texture("icons/save.png", Size(50, 50)), Position(1515, 15));
-
   CREATE(toolbar_listener, ToolbarListener);
 
-  std::unique_ptr<Window> pencil_button_outline(
-      new RectWindow(Size(60, 60), Position(10, 905), Color(80, 90, 91)));
-  std::unique_ptr<Window> eraser_button_outline(
-      new RectWindow(Size(60, 60), Position(10, 975), Color(80, 90, 91)));
-  std::unique_ptr<Window> hue_selector_outline(
-      new RectWindow(Size(370, 40), Position(1540, 1010), Color(80, 90, 91)));
-  std::unique_ptr<Window> sv_selector_outline(
-      new RectWindow(Size(370, 370), Position(1540, 630), Color(80, 90, 91)));
-  std::unique_ptr<Window> save_button_outline(
-      new RectWindow(Size(60, 60), Position(1510, 10), Color(80, 90, 91)));
+  CREATE(pencil_button_sprite, Sprite,
+         Texture("icons/pencil.png", Size(50, 50)), Position(15, 910));
+  CREATE(eraser_button_sprite, Sprite,
+         Texture("icons/eraser.png", Size(50, 50)), Position(15, 980));
+  CREATE(save_button_sprite, Sprite, Texture("icons/save.png", Size(50, 50)),
+         Position(1515, 15));
 
-  std::unique_ptr<Window> pencil_button(new RectButton(
-      Size(50, 50), Position(15, 910), Color(236, 236, 236), PENCIL));
-  std::unique_ptr<Window> eraser_button(new RectButton(
-      Size(50, 50), Position(15, 980), Color(236, 236, 236), ERASER));
-  std::unique_ptr<Window> save_button(new RectButton(
-      Size(50, 50), Position(1515, 15), Color(236, 236, 236), Canvas::SAVE));
+  CREATE(pencil_button_outline, RectWindow, Size(60, 60), Position(10, 905),
+         Color(80, 90, 91));
+  CREATE(eraser_button_outline, RectWindow, Size(60, 60), Position(10, 975),
+         Color(80, 90, 91));
+  CREATE(hue_selector_outline, RectWindow, Size(370, 40), Position(1540, 1010),
+         Color(80, 90, 91));
+  CREATE(sv_selector_outline, RectWindow, Size(370, 370), Position(1540, 630),
+         Color(80, 90, 91));
+  CREATE(save_button_outline, RectWindow, Size(60, 60), Position(1510, 10),
+         Color(80, 90, 91));
 
-  std::unique_ptr<Window> thickness_slider_base(
-      new RectWindow(Size(360, 2), Position(1545, 609), Color(80, 90, 91)));
+  CREATE(pencil_button, RectButton, Size(50, 50), Position(15, 910),
+         Color(236, 236, 236), PENCIL);
+  CREATE(eraser_button, RectButton, Size(50, 50), Position(15, 980),
+         Color(236, 236, 236), ERASER);
+  CREATE(save_button, RectButton, Size(50, 50), Position(1515, 15),
+         Color(236, 236, 236), Canvas::SAVE);
 
-  std::unique_ptr<Window> hue_selector(
-      new HUEselector(Size(360, 30), Position(1545, 1015)));
-  std::unique_ptr<Window> sv_selector(
-      new SVselector(Size(360, 360), Position(1545, 635)));
+  CREATE(hue_selector, HUEselector, Size(360, 30), Position(1545, 1015));
+  CREATE(sv_selector, SVselector, Size(360, 360), Position(1545, 635));
+  CREATE(thickness_slider_base, RectWindow, Size(360, 2), Position(1545, 609),
+         Color(80, 90, 91));
 
-  std::unique_ptr<Window> hue_slider(new Slider(
-      Size(3, 30), Position(1545, 1015), Color(0, 0, 0), 1545, 1905, 1, true));
-  std::unique_ptr<Window> thickness_slider(new Slider(
-      Size(5, 30), Position(1545, 595), Color(0, 0, 0), 1545, 1905, 1, true));
+  CREATE(hue_slider, Slider, Size(3, 30), Position(1545, 1015), Color(0, 0, 0),
+         1545, 1905, 1, true);
+  CREATE(thickness_slider, Slider, Size(5, 30), Position(1545, 595),
+         Color(0, 0, 0), 1545, 1905, 1, true);
+  CREATE(sv_fader, Fader, Size(8, 8), Position(1896, 986), Color(0, 0, 0),
+         Position(1545, 635), Position(1896, 986));
 
-  std::unique_ptr<Window> sv_fader(
-      new Fader(Size(8, 8), Position(1896, 986), Color(0, 0, 0),
-                Position(1545, 635), Position(1896, 986)));
 
-  std::unique_ptr<Window> canvas(
-      new Canvas(Size(1500, 890), Position(0, 0), Color(255, 255, 255)));
+  CREATE(canvas, Canvas, Size(1500, 890), Position(0, 0), Color(255, 255, 255));
 
   SUBSCRIBE(root_window.get(), sv_fader.get());
   SUBSCRIBE(hue_selector.get(), sv_fader.get());
